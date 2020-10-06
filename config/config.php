5d58f3cea5b5a9a1cd087a0884ced1f0333ac63c
Kevin <33708864+kevin499@users.noreply.github.com>
2020-10-05 21:03:03 -0300

    cambios en el config

---
+++ config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+    define( 'URL', 'http://' . $_SERVER['HTTP_HOST'] );
+
+    define( 'HOST', 'localhost' );
+    define( 'DB', 'peliculas' );
+    define( 'USER', 'root' );
+    define( 'password', '' );
+    define( 'CHARSET ', 'utf8mb4' );
