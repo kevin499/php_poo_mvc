30488203e9c0376d56fa0c7652bfdb08f8e319ff
Kevin <33708864+kevin499@users.noreply.github.com>
2020-10-27 13:37:52 -0300

    correcion de errores en la interfaz

--- views/destino/index.php
+++ views/destino/index.php
@@ -89,7 +89,6 @@
     </div>
 </div>
 
-<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 <?php include 'views/footer.php'; ?>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
